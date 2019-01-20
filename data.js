f1ffaea11ebc2ff8d7a3155495914aaa9ff0435a
Jennifer Parra <jparra.biblio@gmail.com>
2019-01-20 16:58:33 -0300

    función ordenar de a-z / z-a - funciona

--- data.js
+++ data.js
@@ -7,5 +7,47 @@ window.allPokemon = {
     return element.type.indexOf(condition) > -1
   })
   return filteredType;
- }
+ },
+
+
+ sortData: (data, sortBy) => {
+  // console.log(data, sortOrder); 
+
+  if (sortBy == 'a-z') {
+
+    return data.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+
+  if (sortBy == 'z-a') {
+    return data.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 }
+}
+
+/* 
+  users.sort(function(a, b){
+    if(a.firstname < b.firstname) { return -1; }
+    if(a.firstname > b.firstname) { return 1; }
+    return 0;
+})
+
+sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. 
+El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice c
+on respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, 
+indica si se quiere ordenar de manera ascendente o descendente. */
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const data = window.POKEMON.pokemon;
 const containerRoot = document.getElementById('root');
 const selectType = document.getElementById('pokeType');
+const selectOrder = document.getElementById('orderBy');
 
-
+//función que muestra
 const showData = (POKEMON) => {
   let result = '';
   const dataPoke = POKEMON.pokemon;
   dataPoke.forEach(element => {
-  result = containerRoot.innerHTML += `
+    result = containerRoot.innerHTML += `
       <div>
         <div class="card">
           <div class="box">
@@ -23,9 +24,9 @@ const showData = (POKEMON) => {
   return result;
 }
 
-
-  selectType.addEventListener('change', () => {
-  let condition = selectType.options[selectType.selectedIndex].value;
+//función que 
+selectType.addEventListener('change', () => {
+  let condition = selectType.value;
   let filtered = window.allPokemon.filterType(data, condition);
   // limpio div
   containerRoot.innerHTML = '';
@@ -46,3 +47,58 @@ const showData = (POKEMON) => {
 });
 
 window.onload = showData(POKEMON);
+
+//ordenar por
+
+selectOrder.addEventListener('change', () => {
+  let sortBy = selectOrder.value;
+  let ordered = window.allPokemon.sortData(data, sortBy);
+  containerRoot.innerHTML = '';
+  ordered.forEach(element => {
+    containerRoot.innerHTML += `
+    <div>
+        <div class="card">
+          <div class="box">
+            <h2>${element.name}</h2>
+            <div class="img">
+                <img src="${element.img}">
+            </div>
+            <h3>${element.type}</h3>
+          </div>
+        </div>
+      </div>`
+  })
+})
+
+
+/*document.getElementById('sort').addEventListener('change', (evento) => {
+  evento.preventDefault();
+
+  //Creo una nueva constante "s   eleccionOrden" donde tomo el valor seleccionado en Select
+  const sorted = document.getElementById('sort').value
+  //variable que  guarda todos los return de "ordenarPokemons" de la data.js
+  let ver = sortData(data, sorted)
+
+  //Se borra lo impreso cada vez que vuelvo a seleccionar otro ordenador, debe ser antes del que se imprime en la pantalla
+  document.getElementById("root").innerHTML = " ";
+
+  //se recorre la variable "ver" con todos los datos de "ordenado"
+  for (let i = 0; i < ver.length; i++) {
+
+      //Se imprime en el id=vacío, la variable ver siendo recorrida. Muestro solo el nombre    
+      document.getElementById("root").innerHTML +=
+          `<div>
+          <div class="card">
+          <div class="box">
+              <div class="img">
+                  <img src="${ver[i].img}">
+              </div>
+              <h3>${ver[i].num}</h3>
+              <h2>${ver[i].name}</h2>
+              <p>${ver[i].type}</p>
+          </div>
+          </div>
+          </div> `
+
+  }
+*/
