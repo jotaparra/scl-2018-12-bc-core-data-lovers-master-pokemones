292e4660df525381aff609cb00282ef277bb6548
Jennifer Parra <jparra.biblio@gmail.com>
2019-01-21 03:52:29 -0300

    footer y enlace en la card para colorear

--- main.js
+++ main.js
@@ -3,38 +3,39 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 const data = window.POKEMON.pokemon;
-const data2 = window.POKEMON;
 const containerRoot = document.getElementById('root');
 const selectType = document.getElementById('pokeType');
 const selectOrder = document.getElementById('orderBy');
 
 //función que muestra
-const showData = (data2) => {
+const showData = (data) => {
   let result = '';
-  const dataPoke = data2.pokemon;
+  const dataPoke = data.pokemon;
   dataPoke.forEach(element => {
-    result = containerRoot.innerHTML += `
-          <div class="col s3 card small card-panel">
+    result = containerRoot.innerHTML += 
+    `
+          <div class="col m3 card small hoverable">
           <div class="card-image waves-effect waves-block waves-light">  
-            <img class="caja-pokemon activator responsive-img" src="${element.img}">
+            <img class="activator responsive-img caja-pokemon" src="${element.img}">
           </div>
           <div class="card-content">
           <p>N° ${element.num}</p>
-          <span class="card-title activator grey-text text-darken-4">${element.name}<i class="material-icons right">add</i></span>
+          <span class="card-title activator grey-text text-darken-4"><span class="bold">${element.name}</span><i class="material-icons right">add</i></span>
           <p>${element.type[0]} ${element.type[1]}</p>
           </div>
           <div class="card-reveal deep-purple lighten-5">
             <span class="card-title grey-text text-darken-4">${element.name}<i class="material-icons right">close</i></span>
-            <p><span class="caracteristicas">Altura:</span> ${element.height}</p>
-            <p><span class="caracteristicas">Peso:</span> ${element.weight}</p>
-            <p><span class="caracteristicas">Debilidades:</span> ${element.weaknesses}</p>
-            <p><span class="caracteristicas">Huevo:</span> ${element.egg}</p>
+            <p><span class="bold">Altura:</span> ${element.height}</p>
+            <p><span class="bold">Peso:</span> ${element.weight}</p>
+            <p><span class="bold">Debilidades:</span> ${element.weaknesses}</p>
+            <p><span class="bold">Huevo:</span> ${element.egg}</p>
+            <a href="http://www.morningkids.net/pokemon/${element.id}-es-pokemon.php" target="_blank">Imagen para colorear</a>
           </div>
         </div>`
   });
   return result;
 }
-
+window.onload = showData(POKEMON);
 
 //función que filtra
 selectType.addEventListener('change', () => {
@@ -43,28 +44,30 @@ selectType.addEventListener('change', () => {
   let countType = window.allPokemon.computeStats(filtered);
   // limpio div
   containerRoot.innerHTML = '';
-  containerRoot.innerHTML = `<p>Los pokemones tipo ${condition} son: ${countType} <p/>`;
+  containerRoot.innerHTML = `<h5>Los pokemones tipo ${condition} son: ${countType} </h5>`;
   filtered.forEach(element => {
     containerRoot.innerHTML += `
-    <div class="col s3 card small card-panel">
-    <div class="card-image waves-effect waves-block waves-light">
-    <div class="caja-pokemon">
-      <img class="activator responsive-img" src="${element.img}">
-    </div>
+    <div class="col m3 card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">  
+      <img class="activator responsive-img caja-pokemon" src="${element.img}">
     </div>
     <div class="card-content">
-    <span class="card-title activator grey-text text-darken-4">${element.name}<i class="material-icons right">add</i></span>
-    <p><a href="#">${element.type}</a></p>
+    <p>N° ${element.num}</p>
+    <span class="card-title activator grey-text text-darken-4"><span class="bold">${element.name}</span><i class="material-icons right">add</i></span>
+    <p>${element.type[0]} ${element.type[1]}</p>
     </div>
-    <div class="card-reveal">
+    <div class="card-reveal deep-purple lighten-5">
       <span class="card-title grey-text text-darken-4">${element.name}<i class="material-icons right">close</i></span>
-      <p class="flow-text">Here is some more information about this product that is only revealed once clicked on.</p>
+      <p><span class="bold">Altura:</span> ${element.height}</p>
+      <p><span class="bold">Peso:</span> ${element.weight}</p>
+      <p><span class="bold">Debilidades:</span> ${element.weaknesses}</p>
+      <p><span class="bold">Huevo:</span> ${element.egg}</p>
     </div>
   </div>`
   })
 });
 
-window.onload = showData(POKEMON);
+
 
 //función que ordena
 
@@ -74,60 +77,22 @@ selectOrder.addEventListener('change', () => {
   containerRoot.innerHTML = '';
   ordered.forEach(element => {
     containerRoot.innerHTML += `
-    <div class="col s3 card small card-panel">
-    <div class="card-image waves-effect waves-block waves-light">
-    <div class="caja-pokemon">
-      <img class="activator responsive-img" src="${element.img}">
-    </div>
+    <div class="col m3 card small hoverable">
+    <div class="card-image waves-effect waves-block waves-light">  
+      <img class="activator responsive-img caja-pokemon" src="${element.img}">
     </div>
     <div class="card-content">
-    <span class="card-title activator grey-text text-darken-4">${element.name}<i class="material-icons right">add</i></span>
-    <p><a href="#">${element.type}</a></p>
+    <p>N° ${element.num}</p>
+    <span class="card-title activator grey-text text-darken-4"><span class="bold">${element.name}</span><i class="material-icons right">add</i></span>
+    <p>${element.type[0]} ${element.type[1]}</p>
     </div>
-    <div class="card-reveal">
+    <div class="card-reveal deep-purple lighten-5">
       <span class="card-title grey-text text-darken-4">${element.name}<i class="material-icons right">close</i></span>
-      <p class="flow-text">Here is some more information about this product that is only revealed once clicked on.</p>
+      <p><span class="bold">Altura:</span> ${element.height}</p>
+      <p><span class="bold">Peso:</span> ${element.weight}</p>
+      <p><span class="bold">Debilidades:</span> ${element.weaknesses}</p>
+      <p><span class="bold">Huevo:</span> ${element.egg}</p>
     </div>
   </div>`
   })
 })
-
-//Calcula el total de pokemones del tipo seleccionado
-
-
-
-
-
-
-
-/*document.getElementById('sort').addEventListener('change', (evento) => {
-  evento.preventDefault();
-
-  //Creo una nueva constante "s   eleccionOrden" donde tomo el valor seleccionado en Select
-  const sorted = document.getElementById('sort').value
-  //variable que  guarda todos los return de "ordenarPokemons" de la data.js
-  let ver = sortData(data, sorted)
-
-  //Se borra lo impreso cada vez que vuelvo a seleccionar otro ordenador, debe ser antes del que se imprime en la pantalla
-  document.getElementById("root").innerHTML = " ";
-
-  //se recorre la variable "ver" con todos los datos de "ordenado"
-  for (let i = 0; i < ver.length; i++) {
-
-      //Se imprime en el id=vacío, la variable ver siendo recorrida. Muestro solo el nombre    
-      document.getElementById("root").innerHTML +=
-          `<div>
-          <div class="card">
-          <div class="box">
-              <div class="img">
-                  <img src="${ver[i].img}">
-              </div>
-              <h3>${ver[i].num}</h3>
-              <h2>${ver[i].name}</h2>
-              <p>${ver[i].type}</p>
-          </div>
-          </div>
-          </div> `
-
-  }
-*/
